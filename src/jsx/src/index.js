255eaa196ec9fb03f4635b8118d19255dc16fad1
Tony Joanes <tony.joanes@mews.com>
2023-01-16 21:36:56 +0000

    finish section 2

--- src/jsx/src/index.js
+++ src/jsx/src/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
-
-function App() {
-    const inputType = 'number';
-    const minValue = 5;
-
-    return (
-        <div className="wrapper">
-            <textarea
-                readOnly={true}
-                maxLength={3}
-                spellCheck={true}
-                style={{ backgroundColor: 'gray' }}
-            />
-        </div>
-    )
-}
 
 root.render(<App />);
